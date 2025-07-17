1faa25fa997cf779eb7b80690e423a7f3e7efb11
Gokul S <gokul.sureshkumar06@gmail.com>
2025-07-18 00:01:59 +0530

    background grid box

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -8,12 +8,23 @@ const Layout = ({ children }) => {
     return (
         <div className='layout_container'>
             <div className='layout_wrapper'>
-                <Gutter>
-                    <div className='layout_header'><Menu /></div>
-                    <div className='layout_body'>{children}</div>
-                    <div className='layout_footer'><Footer /></div>
-                </Gutter>
-                
+                <div className="relative h-full w-full bg-slate-950">
+                    <div
+                        className="absolute bottom-0 left-0 right-0 top-0"
+                        style={{
+                            backgroundImage: `linear-gradient(to right, #4f4f4f2e 1px, transparent 1px),
+                                            linear-gradient(to bottom, #4f4f4f2e 1px, transparent 1px)`,
+                            backgroundSize: '6.5rem 5rem',
+                            backgroundRepeat: 'repeat',
+                        }}
+                    >
+                        <Gutter>
+                            <div className='layout_header'><Menu /></div>
+                            <div className='layout_body'>{children}</div>
+                            <div className='layout_footer'><Footer /></div>
+                        </Gutter>
+                    </div>
+                </div>
             </div>
         </div>
     )
